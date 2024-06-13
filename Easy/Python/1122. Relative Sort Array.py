87a49ea3cc3bbd9e8d0f9c7fe5d695442c88fef8
djg-91 <jose.constenla@gmail.com>
2024-06-13 10:44:39 +0200

    feat: add problem '1122. Relative Sort Array'

---
+++ Easy/Python/1122. Relative Sort Array.py
@@ -0,0 +1,50 @@
+'''
+https://leetcode.com/problems/relative-sort-array
+
+    Given two arrays arr1 and arr2, the elements of arr2 are distinct, 
+    and all elements in arr2 are also in arr1.
+
+    Sort the elements of arr1 such that the relative ordering of items 
+    in arr1 are the same as in arr2. Elements that do not appear in arr2 
+    should be placed at the end of arr1 in ascending order.
+
+    Example 1:
+
+        Input: arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+        Output: [2,2,2,1,4,3,3,9,6,7,19]
+    
+    Example 2:
+
+        Input: arr1 = [28,6,22,8,44,17], arr2 = [22,28,8,6]
+        Output: [22,28,8,6,17,44]
+    
+    Constraints:
+
+        1 <= arr1.length, arr2.length <= 1000
+        0 <= arr1[i], arr2[i] <= 1000
+        All the elements of arr2 are distinct.
+        Each arr2[i] is in arr1.
+'''
+
+from typing import List
+from collections import Counter
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        list_in, list_not = [], []
+
+        for num in arr1:
+            if num in arr2:
+                list_in.append(num)
+            else:
+                list_not.append(num)
+
+        list_not.sort()
+
+        counter = Counter(list_in)
+        res = []
+
+        for num in arr2:
+            res += [num] * counter[num]
+
+        return res + list_not
