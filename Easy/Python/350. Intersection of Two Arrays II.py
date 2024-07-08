6febd3260ae1528b775f37d9a5d3ca70e42d0f7f
djg-91 <jose.constenla@gmail.com>
2024-07-08 04:52:30 +0200

    feat: add problem '350. Intersection of Two Arrays II'

---
+++ Easy/Python/350. Intersection of Two Arrays II.py
@@ -0,0 +1,43 @@
+'''
+https://leetcode.com/problems/intersection-of-two-arrays-ii
+
+    Given two integer arrays nums1 and nums2, return an array of their intersection. 
+    Each element in the result must appear as many times as it shows in both arrays 
+    and you may return the result in any order.
+
+    Example 1:
+
+    Input: nums1 = [1,2,2,1], nums2 = [2,2]
+    Output: [2,2]
+
+    Example 2:
+
+    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    Output: [4,9]
+    Explanation: [9,4] is also accepted.
+    
+    Constraints:
+
+    1 <= nums1.length, nums2.length <= 1000
+    0 <= nums1[i], nums2[i] <= 1000
+'''
+
+
+from typing import List
+from collections import Counter
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums2) > len(nums1):
+            n1, n2 = nums1, nums2
+        else:
+            n1, n2 = nums2, nums1
+        
+        n1 = Counter(n1)
+        n2 = Counter(n2)
+
+        return [k for k, v in n1.items() if k in n2 for _ in range(min(v, n2[k]))]
+
+
+if __name__ == '__main__':
+    print(Solution().intersect([4,9,5], [9,4,9,8,4]))
