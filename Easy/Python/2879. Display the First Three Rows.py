6b3e3b0a316b371b3ee6b95bc862c4ce71a21189
djg-91 <jose.constenla@gmail.com>
2024-07-31 19:14:08 +0200

    feat: add problem '2879. Display the First Three Rows'

---
+++ Easy/Python/2879. Display the First Three Rows.py
@@ -0,0 +1,48 @@
+'''
+https://leetcode.com/problems/display-the-first-three-rows
+
+    DataFrame: employees
+        +-------------+--------+
+        | Column Name | Type   |
+        +-------------+--------+
+        | employee_id | int    |
+        | name        | object |
+        | department  | object |
+        | salary      | int    |
+        +-------------+--------+
+
+    Write a solution to display the first 3 rows of this DataFrame.
+
+    Example 1:
+
+        Input:
+            DataFrame employees
+            +-------------+-----------+-----------------------+--------+
+            | employee_id | name      | department            | salary |
+            +-------------+-----------+-----------------------+--------+
+            | 3           | Bob       | Operations            | 48675  |
+            | 90          | Alice     | Sales                 | 11096  |
+            | 9           | Tatiana   | Engineering           | 33805  |
+            | 60          | Annabelle | InformationTechnology | 37678  |
+            | 49          | Jonathan  | HumanResources        | 23793  |
+            | 43          | Khaled    | Administration        | 40454  |
+            +-------------+-----------+-----------------------+--------+
+        Output:
+            +-------------+---------+-------------+--------+
+            | employee_id | name    | department  | salary |
+            +-------------+---------+-------------+--------+
+            | 3           | Bob     | Operations  | 48675  |
+            | 90          | Alice   | Sales       | 11096  |
+            | 9           | Tatiana | Engineering | 33805  |
+            +-------------+---------+-------------+--------+
+
+        Explanation: 
+            Only the first 3 rows are displayed.
+'''
+
+
+import pandas as pd
+
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    return employees[:3]
